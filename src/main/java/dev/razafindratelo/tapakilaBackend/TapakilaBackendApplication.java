8614e53cd28ce2831aeba10fa3b82887583207cd
Abega1642 <hei.abega.2@gmail.com>
2025-02-25 15:54:44 +0300

    feat: initialize tapakila backend application

---
+++ src/main/java/dev/razafindratelo/tapakilaBackend/TapakilaBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.razafindratelo.tapakilaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapakilaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapakilaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/razafindratelo/tapakilaBackend/TapakilaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.razafindratelo.tapakilaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapakilaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
