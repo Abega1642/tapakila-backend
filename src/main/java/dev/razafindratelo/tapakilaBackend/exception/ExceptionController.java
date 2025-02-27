9019045d9d2a18f33df60be83449ca152c85c089
Abega1642 <hei.abega.2@gmail.com>
2025-02-27 12:04:27 +0300

    feat: add ExceptionController

---
+++ src/main/java/dev/razafindratelo/tapakilaBackend/exception/ExceptionController.java
@@ -0,0 +1,60 @@
+package dev.razafindratelo.tapakilaBackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(NotImplementedException.class)
+    public ResponseEntity<ExceptionMessage> handleNotImplementedException(NotImplementedException notImplemented) {
+        ExceptionMessage em = new ExceptionMessage(
+                notImplemented.getMessage(),
+                HttpStatus.NOT_IMPLEMENTED.value(),
+                HttpStatus.NOT_IMPLEMENTED
+        );
+        return new ResponseEntity<>(em, HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @ExceptionHandler(ActionNotAllowedException.class)
+    public ResponseEntity<ExceptionMessage> handleNotAllowedException(ActionNotAllowedException notAllowed) {
+        ExceptionMessage em = new ExceptionMessage(
+                notAllowed.getMessage(),
+                HttpStatus.FORBIDDEN.value(),
+                HttpStatus.FORBIDDEN
+        );
+        return new ResponseEntity<>(em, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(ResourceDuplicatedException.class)
+    public ResponseEntity<ExceptionMessage> handleResourceDuplicatedException(ResourceDuplicatedException duplicated) {
+        ExceptionMessage em = new ExceptionMessage(
+                duplicated.getMessage(),
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT
+        );
+        return new ResponseEntity<>(em, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ExceptionMessage> handleResourceNotFoundException(ResourceNotFoundException notFound) {
+        ExceptionMessage em = new ExceptionMessage(
+                notFound.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND
+        );
+        return new ResponseEntity<>(em, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ExceptionMessage> handleBadRequestException(BadRequestException badRequest) {
+        ExceptionMessage em = new ExceptionMessage(
+                badRequest.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST
+        );
+        return new ResponseEntity<>(em, HttpStatus.BAD_REQUEST);
+    }
+}
