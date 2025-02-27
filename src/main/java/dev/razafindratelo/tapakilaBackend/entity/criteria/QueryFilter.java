c6c8415f502eefc925a33cc881a22303b6827724
Abega1642 <hei.abega.2@gmail.com>
2025-02-27 13:51:16 +0300

    refactor: rename RequestFilter into QueryFilter

--- src/main/java/dev/razafindratelo/tapakilaBackend/entity/criteria/QueryFilter.java
+++ src/main/java/dev/razafindratelo/tapakilaBackend/entity/criteria/QueryFilter.java
@@ -4,9 +4,9 @@ import dev.razafindratelo.tapakilaBackend.entity.criteria.enums.AvailableColumn;
 import dev.razafindratelo.tapakilaBackend.entity.criteria.enums.OperatorType;
 import dev.razafindratelo.tapakilaBackend.entity.criteria.enums.ValueType;
 
-public class RequestFilter extends Filter {
+public class QueryFilter extends Filter {
 
-    public RequestFilter(AvailableColumn column, OperatorType operatorType, Object value) {
+    public QueryFilter(AvailableColumn column, OperatorType operatorType, Object value) {
         super(column, operatorType, value, ValueType.REQUEST);
     }
 
