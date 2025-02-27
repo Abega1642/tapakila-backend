4120744625f925e30a45bc262a46f36e491e5463
Abega1642 <hei.abega.2@gmail.com>
2025-02-27 11:47:05 +0300

    feat: add DataSource

---
+++ src/main/java/dev/razafindratelo/tapakilaBackend/dao/DataSource.java
@@ -0,0 +1,31 @@
+package dev.razafindratelo.tapakilaBackend.dao;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Slf4j
+public class DataSource {
+    private final String url;
+    private final String username;
+    private final String password;
+
+    public DataSource() {
+        Dotenv dotenv = Dotenv.load();
+        this.url = dotenv.get("DB_URL");
+        this.username = dotenv.get("DB_USERNAME");
+        this.password = dotenv.get("DB_PASSWORD");
+    }
+
+    public Connection getConnection(){
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            log.info("Connection established !");
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
