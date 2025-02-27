b41a2014f68b436eb914b95096db46d64a87c8f3
Abega1642 <hei.abega.2@gmail.com>
2025-02-27 12:23:07 +0300

    feat: add all entities DAOs

---
+++ src/main/java/dev/razafindratelo/tapakilaBackend/dao/UserDao.java
@@ -0,0 +1,48 @@
+package dev.razafindratelo.tapakilaBackend.dao;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import dev.razafindratelo.tapakilaBackend.entity.User;
+import dev.razafindratelo.tapakilaBackend.entity.criteria.Criteria;
+import dev.razafindratelo.tapakilaBackend.exception.NotImplementedException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor
+@Getter
+public class UserDao implements DAO<User> {
+    private final DataSource dataSource;
+
+    @Override
+    public User save(User entity) {
+        throw new NotImplementedException("Saving user not implemented yet");
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        throw new NotImplementedException("Finding user by id not implemented yet");
+    }
+
+    @Override
+    public List<User> findAll(long page, long size) {
+        throw new NotImplementedException("Finding all users not implemented yet");
+    }
+
+    @Override
+    public List<User> findAllByCriteria(List<Criteria> criteria, long page, long size) {
+        throw new NotImplementedException("Finding all users by criteria not implemented yet");
+    }
+
+    @Override
+    public User update(String id, User entity) {
+        throw new NotImplementedException("Updating user not implemented yet");
+    }
+
+    @Override
+    public Optional<User> delete(String id) {
+        throw new NotImplementedException("Deleting user not implemented yet");
+    }
+}
