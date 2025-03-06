9225fe0825657ba60d61b59730120d07abab1ede
Abega1642 <hei.abega.2@gmail.com>
2025-03-06 16:12:39 +0300

    feat: add @JsonFormat and new attributes

--- src/main/java/dev/razafindratelo/tapakilaBackend/entity/TicketPriceInfo.java
+++ src/main/java/dev/razafindratelo/tapakilaBackend/entity/TicketPriceInfo.java
@@ -1,6 +1,8 @@
 package dev.razafindratelo.tapakilaBackend.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.razafindratelo.tapakilaBackend.entity.enums.Currency;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -16,8 +18,13 @@ import java.time.LocalDateTime;
 public class TicketPriceInfo {
     @JsonProperty("id")  private String id;
     @JsonProperty("price") private double price;
-    @JsonProperty("created_at") private LocalDateTime createdAt;
+    @JsonProperty("currency") private Currency currency;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
     @JsonProperty("max_number") private long maxNumber;
-    @JsonProperty("type") private Ticket ticketType;
+    @JsonProperty("corresponding_ticket_type") private Ticket ticketType;
+    @JsonProperty("left_tickets") private long leftTickets;
     @JsonProperty("associated_event_id") private String associatedEventId;
 }
