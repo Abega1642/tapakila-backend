c995dc75284baff956cd7d4defa2794902fb7a21
Abega1642 <hei.abega.2@gmail.com>
2025-03-25 16:37:21 +0300

    feat(controller): add role filter in EventTurnoverController and TotalSOldTicketController

--- src/main/java/dev/razafindratelo/tapakilaBackend/controller/TotalSoldTicketController.java
+++ src/main/java/dev/razafindratelo/tapakilaBackend/controller/TotalSoldTicketController.java
@@ -6,6 +6,7 @@ import dev.razafindratelo.tapakilaBackend.entity.TotalSoldTicket;
 import dev.razafindratelo.tapakilaBackend.service.totalSoldTicketService.TotalSoldTicketService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.time.Month;
 import java.time.Year;
@@ -17,16 +18,19 @@ import java.util.List;
 public class TotalSoldTicketController {
     private final TotalSoldTicketService totalSoldTicketService;
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("current-year")
     public ResponseEntity<List<SoldTicketStatOverYear>> getTicketsStatOfCurrentYear() {
         return ResponseEntity.ok(totalSoldTicketService.getSoldTicketStatOverYearOfCurrentYear());
     }
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("current-month")
     public ResponseEntity<List<TotalSoldTicket>> getTotalSoldTicketStatOfCurrentMonth() {
         return ResponseEntity.ok(totalSoldTicketService.getTotalSoldTicketOfCurrentMonth());
     }
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("month/{month}/of-year/{year}")
     public ResponseEntity<List<TotalSoldTicket>> getTotalSoldTicketStatOfMonth(
             @PathVariable("year") Year year,
@@ -35,6 +39,7 @@ public class TotalSoldTicketController {
         return ResponseEntity.ok(totalSoldTicketService.getTotalSoldTicketStatOfMonth(month, year));
     }
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("over-year-range")
     public ResponseEntity<List<SoldTicketStatOverYearRange>> getTotalSOldTicketStatOverYearRange(
             @RequestParam("startYear") Year startYear,
