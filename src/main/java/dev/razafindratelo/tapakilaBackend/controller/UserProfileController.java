7715bd81900f0700c2742a9579042665f702f965
Abega1642 <hei.abega.2@gmail.com>
2025-03-25 16:38:32 +0300

    feat(UserController): remove update method for user profile

--- src/main/java/dev/razafindratelo/tapakilaBackend/controller/UserProfileController.java
+++ src/main/java/dev/razafindratelo/tapakilaBackend/controller/UserProfileController.java
@@ -1,24 +1,32 @@
 package dev.razafindratelo.tapakilaBackend.controller;
 
-import dev.razafindratelo.tapakilaBackend.service.userProfileService.UserProfileService;
+import dev.razafindratelo.tapakilaBackend.service.imgServices.userProfileService.UserProfileService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/user/profile")
 public class UserProfileController {
     private final UserProfileService userProfileService;
 
-    @GetMapping("/user/profile/{userEmail}")
+    @GetMapping("/{userEmail}")
     public ResponseEntity<byte[]> getUserProfile(@PathVariable("userEmail") String userEmail) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .contentType(MediaType.IMAGE_PNG)
                 .body(userProfileService.findUserProfileByUserEmail(userEmail));
+    }
+
+    @PostMapping("/update/{userEmail}")
+    public ResponseEntity<String> updateUserProfile(
+            @PathVariable("userEmail") String userEmail,
+            @RequestBody MultipartFile profile
+    ) {
+        return ResponseEntity.ok(userProfileService.updateProfileImageByUserEmail(userEmail, profile));
     }
 }
