1c5c02ff1fecc1bcac30b2e484cc373e9f06e70f
Abega1642 <hei.abega.2@gmail.com>
2025-03-25 16:35:17 +0300

    feat(FileDao): add methods related to event img

--- src/main/java/dev/razafindratelo/tapakilaBackend/dao/FileDao.java
+++ src/main/java/dev/razafindratelo/tapakilaBackend/dao/FileDao.java
@@ -38,6 +38,28 @@ public class FileDao {
         }
     }
 
+    public String updateProfileImageByUserEmail(String email, String imagePath) {
+        Connection connection = dataSource.getConnection(FileDao.class.getName());
+        String sqlRequest =
+                """
+                UPDATE "user" SET profile_img_path = ? WHERE email = ?
+                """;
+        try (PreparedStatement sqlStmt = connection.prepareStatement(sqlRequest)) {
+            sqlStmt.setString(1, imagePath);
+            sqlStmt.setString(2, email);
+
+            if (sqlStmt.executeUpdate() > 0)
+                return imagePath;
+
+            throw new SQLException("FileDao.updateProfileImageByUserEmail:: could not update profile img path");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(
+                    "FileDao.updateProfileImageByUserEmail :: " + e.getMessage()
+            );
+        }
+    }
+
     public String findQRCodePath(String email) {
         String sqlRequest =
                 """
@@ -52,6 +74,53 @@ public class FileDao {
         } catch (SQLException e) {
             throw new RuntimeException(
                     "FileDao.findProfileByUserEmail :: " + e.getMessage()
+            );
+        }
+    }
+
+    public String findEventImgById(String id) {
+        String sqlRequest =
+                """
+                SELECT image_path FROM event WHERE id = ?
+                """;
+
+        Connection connection = dataSource.getConnection(FileDao.class.getName());
+
+        try(PreparedStatement sqlStmt = connection.prepareStatement(sqlRequest)) {
+
+            sqlStmt.setString(1, id);
+            ResultSet resultSet = sqlStmt.executeQuery();
+
+            if (resultSet.next())
+                return resultSet.getString(1);
+
+            throw new SQLException("FileDao.findEventImgById :: could not event img path");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(
+                    "FileDao.findEventImgById :: " + e.getMessage()
+            );
+        }
+    }
+
+    public String updateEventImgByIdAndPath(String path, String eventId) {
+        Connection connection = dataSource.getConnection(FileDao.class.getName());
+        String sqlRequest =
+                """
+                UPDATE event SET image_path = ? WHERE id = ?
+                """;
+        try (PreparedStatement sqlStmt = connection.prepareStatement(sqlRequest)) {
+            sqlStmt.setString(1, path);
+            sqlStmt.setString(2, eventId);
+
+            if (sqlStmt.executeUpdate() > 0)
+                return path;
+
+            throw new SQLException("FileDao.updateEventImgByIdAndPath:: could not update event img path");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(
+                    "FileDao.updateEventImgByIdAndPath :: " + e.getMessage()
             );
         }
     }
