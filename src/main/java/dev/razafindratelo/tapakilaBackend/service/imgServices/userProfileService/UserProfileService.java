6b4b5b73b82f47623e8cfc8f00e36dc90be7ccdf
Abega1642 <hei.abega.2@gmail.com>
2025-03-25 16:33:59 +0300

    feat(service): add imgService

--- src/main/java/dev/razafindratelo/tapakilaBackend/service/imgServices/userProfileService/UserProfileService.java
+++ src/main/java/dev/razafindratelo/tapakilaBackend/service/imgServices/userProfileService/UserProfileService.java
@@ -1,4 +1,4 @@
-package dev.razafindratelo.tapakilaBackend.service.userProfileService;
+package dev.razafindratelo.tapakilaBackend.service.imgServices.userProfileService;
 
 import dev.razafindratelo.tapakilaBackend.entity.User;
 import org.springframework.stereotype.Service;
@@ -6,6 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public interface UserProfileService {
-    byte[] findUserProfileByUserEmail(String userEmail);
+    byte[] findUserProfileByUserEmail(String path);
+    String updateProfileImageByUserEmail(String email, MultipartFile file);
     String saveUserProfileByImagePath(MultipartFile file, User user);
 }
